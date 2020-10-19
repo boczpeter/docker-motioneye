e4d5d14bb915678c0883d32058bb0fb45e69ea9e
boca <boczpeter@gmail.com>
2020-10-19 22:27:49 +0200

    init

--- run.sh
+++ run.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-docker run -d \
- --device=/dev/video0                      # (optional) if you will use a local v4l camera \
- -p 8765:8765                              # web interface of MotionEye \
- -e TZ="Europe/Budapest"                   # your timezone \
- -v ./conf:/etc/motioneye  # configuration files \
- -v ./camera:/var/lib/motioneye   # captured movies and pictures \
- -e UID=1000                               # User ID \
- -e GID=1000                               # Group ID \
- djaydev/motioneye
+docker run -d --device=/dev/video0 -p 8765:8765 -e TZ="Europe/Budapest" -v ./conf:/etc/motioneye  -v ./camera:/var/lib/motioneye -e UID=1000 -e GID=1000  djaydev/motioneye
