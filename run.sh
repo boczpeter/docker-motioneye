4dfca3f7ed02dd3e8e1741bbf9ca4fde798bfc88
Péter Bócz <boczpeter@gmail.com>
2020-10-19 23:26:05 +0200

    1st succesful start

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DIR=$(dirname $0)
+DIR=$(realpath $(dirname $0))
 
-docker run -d --device=/dev/video0 -p 8765:8765 -e TZ="Europe/Budapest" -v $DIR/conf:/etc/motioneye  -v $DIR/camera:/var/lib/motioneye -e UID=1000 -e GID=1000  djaydev/motioneye
+docker run -d --device=/dev/video0 -p 18765:8765 -e TZ="Europe/Budapest" -v $DIR/conf:/etc/motioneye  -v $DIR/camera:/var/lib/motioneye  djaydev/motioneye
