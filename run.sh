aa6a247add0ab9525a4f3100cff3ee64154b91d2
boca <boczpeter@gmail.com>
2020-10-20 17:21:41 +0200

    1st run based on official image

--- run.sh
+++ run.sh
@@ -2,4 +2,15 @@
 
 DIR=$(realpath $(dirname $0))
 
-docker run -d --device=/dev/video0 -p 18765:8765 -e TZ="Europe/Budapest" -v $DIR/conf:/etc/motioneye  -v $DIR/camera:/var/lib/motioneye  djaydev/motioneye
+docker stop motioneye
+docker container prune -f
+docker run -d \
+    --name motioneye \
+    --hostname motioneye \
+    --device=/dev/video0 \
+    -p 18765:8765 \
+    -e TZ="Europe/Budapest" \
+    -v $DIR/conf:/etc/motioneye \
+    -v $DIR/camera:/var/lib/motioneye \
+    ccrisan/motioneye:master-amd64
+    # djaydev/motioneye
